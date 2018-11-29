591f0dcdc44b600a334cec92fc955301199af7bf
Antoine Bursaux <bursauxa@gmail.com>
2018-11-29 11:57:49 -0500

    Componentized token display.

--- client/src/components/TokenComponent.vue
+++ client/src/components/TokenComponent.vue
@@ -1,3 +1,23 @@
-.token {
-    
+<template>
+    <svg class="token">
+        <rect width=100 height=100 x=14 v-on:click="token.changeTone()" :class="token.tone"></rect>
+        <g v-on:click="token.rotate()" :transform="'rotate(' + token.orientationAsDegrees + ', 64, 50)'">
+            <circle r=20 cx=64 cy=50></circle>
+            <polygon points="64,33 74,64 54,64" ></polygon>
+        </g>
+        <text x=64 y=114>{{ token.id }}</text>
+    </svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import TokenModel from '@/models/TokenModel';
+
+@Component
+export default class TokenComponent extends Vue {
+    @Prop() private token!: TokenModel;
 }
+</script>
+
+<style lang="less" src="./TokenComponent.vue.less">
+</style>
--- client/src/views/Tokens.vue
+++ client/src/views/Tokens.vue
@@ -3,14 +3,7 @@
     <h1>Tokens</h1>
 
 		<svg width=512 height=128>    
-      <svg class="token" v-for="(datum, index) in data" :key="datum.id" :x="index * 128">
-        <rect width=100 height=100 x=14 v-on:click="datum.changeTone()" :class="datum.tone"></rect>
-        <g v-on:click="datum.rotate()" :transform="'rotate(' + datum.orientationAsDegrees + ', 64, 50)'">
-          <circle r=20 cx=64 cy=50></circle>
-          <polygon points="64,33 74,64 54,64" ></polygon>
-        </g>
-        <text x=64 y=114>{{ datum.id }}</text>
-      </svg>
+      <TokenComponent v-for="(datum, index) in data" :key="datum.id" :x="index * 128" :token="datum"/>
   	</svg>
 	</div>
 </template>
@@ -18,8 +11,13 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import TokenModel from '@/models/TokenModel';
+import TokenComponent from '@/components/TokenComponent.vue'
 
-@Component
+@Component({
+  components: {
+    TokenComponent
+  }
+})
 export default class TokensVue extends Vue {
   public data: TokenModel[] = [
     new TokenModel('castle'),
@@ -30,5 +28,8 @@ export default class TokensVue extends Vue {
 }
 </script>
 
-<style lang="less" src="./Tokens.vue.less">
+<style lang="css" scoped>
+.tokens {
+  user-select: none;
+}
 </style>
